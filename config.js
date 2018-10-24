99388088e73216bc45dd00dcad12aa939d7302d8
Lee0701 <hgl1002@naver.com>
2018-10-24 16:17:08 +0900

    Re-add port to config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   token: process.env.BOT_TOKEN,
   botId: process.env.BOT_NAME,
   botAdmin: process.env.BOT_ADMIN,
-  sheetsApiKey: process.env.SHEETS_API_KEY,
+  port: process.env.PORT,
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
   googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
   sheetId: process.env.SHEET_ID
